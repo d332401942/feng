f8af78842f99002a81f2af933a94d281d4602bb1
denghf <hfdend@gmail.com>
2013-06-18 11:50:04 +0800

    msg

--- lib/core/request.php
+++ lib/core/request.php
@@ -6,16 +6,18 @@ class RequestCoreLib extends Feng
     public function httpRequest($parameters,$hostHeader = null)
     {
         $defFunc = Config::VIEW_FUNC;
-        
         $viewStr = empty($parameters[0]) ? strtolower(Config::VIEW_DOLDER) : strtolower($parameters[0]);
         if ($hostHeader && $hostHeader != 'www')
         {
         	$viewStr = $hostHeader . '/' . $viewStr;
         }
         $paramStr = empty($parameters[1]) ? '' : $parameters[1];
+        
         $params = $this->getParams($paramStr);
+        
         $className = null;
         UrlCoreLib::$viewClass = $this->getViewClass($viewStr, $className);
+        
         $templateFile = self::getTempateFile($className);
         if (!UrlCoreLib::$viewClass->isRender())
         {
@@ -28,6 +30,37 @@ class RequestCoreLib extends Feng
         {
             UrlCoreLib::$viewClass->display();
         }
+    }
+    
+    public function hostRequest($parameters,$hostHeader = null)
+    {
+    	$viewStr = array_pop($parameters);
+    	$defFunc = Config::VIEW_FUNC;
+    	if ($hostHeader && $hostHeader != 'www')
+    	{
+    		$viewStr = $hostHeader . '/' . $viewStr;
+    	}
+    	$arr = pathinfo($viewStr);
+    	if (!empty($arr['extension']))
+    	{
+    		throw new Exception('not found :: '.$viewStr);
+    	}
+    	$viewStr = trim($viewStr, '/');
+    	$className = null;
+    	$params = $this->getParameters($viewStr, $className);
+    	$templateFile = self::getTempateFile($className);
+    	if (!UrlCoreLib::$viewClass->isRender())
+    	{
+    		UrlCoreLib::$viewClass->render ( $templateFile );
+    	}
+    	LogVendorLib::start($className, $defFunc);
+    	UrlCoreLib::$viewClass->$defFunc($params);
+    	LogVendorLib::end($className, $defFunc);
+    	if (!UrlCoreLib::$viewClass->isDisplay)
+    	{
+    		UrlCoreLib::$viewClass->display();
+    	}
+    	
     }
 
 	public function ajaxRequest($parameters)
@@ -66,38 +99,101 @@ class RequestCoreLib extends Feng
     }
 
 
-    protected function getViewClass($str, &$className)
+    protected function getParameters($str, &$className)
     {
         $viewArr = explode('/', $str);
-        if (count($viewArr) == 1)
-        {
-            array_push($viewArr, Config::VIEW_FILE);
-        }
         $appDir = APP_DIR;
         if ($appDir == '')
         {
-            $appDir = '.';
+        	$appDir = '.';
         }
         $path = rtrim($appDir, '/') . '/' . Config::VIEW_FOLDER;
-        $preClassName = ucfirst(array_pop($viewArr));
-        $lastClassName = null;
-        foreach ($viewArr as $val)
+        $className = ucfirst(Config::VIEW_FOLDER);
+        $isFind = false;
+        $parameters = array();
+        for ($i = 0; $i < count($viewArr); $i++)
         {
-            $path .= '/' . $val;
-            $lastClassName = ucfirst($val) . $lastClassName;
+        	if (!$isFind)
+        	{
+        		$path .= '/' . $viewArr[$i];
+        		$className = ucfirst($viewArr[$i]) . $className;
+        		if (file_exists($path . '.php'))
+        		{
+        			$isFind = true;
+        		}
+        	}
+        	else
+        	{
+        		$key = $viewArr[$i];
+        		$val = null;
+        		if (!empty($viewArr[$i+1]))
+        		{
+        			$val = $viewArr[$i+1];
+        		}
+        		$parameters[$key] = $val;
+        		$i++;
+        	}
         }
-        $className = $preClassName . $lastClassName . ucfirst(Config::VIEW_FOLDER);
-        $path = $path . '/' . strtolower($preClassName) . '.php';
-        if (!file_exists($path))
+        if ($i == count($viewArr))
         {
-            LogVendorLib::setWarning($path . '没有找到');
-            throw new Exception('not found :: '.$path);
+        	$path = rtrim($appDir, '/') . '/' . Config::VIEW_FOLDER;
+        	$folder = array_shift($viewArr);
+        	$path .= '/' . $folder .'/'. Config::VIEW_FILE . '.php';
+        	if (!file_exists($path))
+        	{
+        		LogVendorLib::setWarning($path . '没有找到');
+        		throw new Exception('not found :: '.$path);
+        	}
+        	$className = ucfirst(Config::VIEW_FILE) . ucfirst($folder) . ucfirst(Config::VIEW_FOLDER);
+        	for ($k = 0; $k < count($viewArr); $k++)
+        	{
+        		$key = $viewArr[$k];
+        		$val = null;
+        		if (!empty($viewArr[$k+1]))
+        		{
+        			$val = $viewArr[$k+1];
+        		}
+        		$parameters[$key] = $val;
+        		$k++;
+        	}
         }
-        include_once $path;
-        array_push(LogVendorLib::$fireDebugInfo['加载文件'], $path);
         UrlCoreLib::$viewClass = new $className($className);
-        return UrlCoreLib::$viewClass;
+        return $parameters;
     }
+    
+    protected function getViewClass($str, &$className)
+    {
+    	$viewArr = explode('/', $str);
+    	if (count($viewArr) == 1)
+    	{
+    		array_push($viewArr, Config::VIEW_FILE);
+    	}
+    	$appDir = APP_DIR;
+    	if ($appDir == '')
+    	{
+    		$appDir = '.';
+    	}
+    	$path = rtrim($appDir, '/') . '/' . Config::VIEW_FOLDER;
+    	$preClassName = ucfirst(array_pop($viewArr));
+    	$lastClassName = null;
+    	foreach ($viewArr as $val)
+    	{
+    		$path .= '/' . $val;
+    		$lastClassName = ucfirst($val) . $lastClassName;
+    	}
+    	$className = $preClassName . $lastClassName . ucfirst(Config::VIEW_FOLDER);
+    	$path = $path . '/' . strtolower($preClassName) . '.php';
+    	if (!file_exists($path))
+    	{
+    		LogVendorLib::setWarning($path . '没有找到');
+    		throw new Exception('not found :: '.$path);
+    	}
+    	include_once $path;
+    	array_push(LogVendorLib::$fireDebugInfo['加载文件'], $path);
+    	UrlCoreLib::$viewClass = new $className($className);
+    	return UrlCoreLib::$viewClass;
+    }
+    
 
     protected function getParams($str)
     {
